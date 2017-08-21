f7071ad2e0060c498012f39382156bb900c3c0fc
nl1644 <10424lisa@gmail.com>
2017-08-20 21:38:15 -0700

    update to drawFlower

--- drawFlower.py
+++ drawFlower.py
@@ -1,15 +1,26 @@
 import turtle
 flower = turtle.Turtle()
 flower.color('blue')
+flower.speed(0)
 
 window = turtle.Screen()
 window.bgcolor('bisque')
 
-flower.forward(50)
-flower.right(120)
-flower.forward(50)
-flower.right(120)
-flower.forward(50)
-flower.right(120)
+
+for i in range (0,50):      
+    flower.right(60)
+    flower.forward(50)
+    flower.right(60)
+    flower.forward(50)
+    flower.right(120)
+    flower.forward(50)
+    flower.right(60)
+    flower.forward(50)
+    flower.right(60)
+    flower.right(7.2)
+flower.color('green')
+flower.right(90)
+flower.forward(300)
 
 window.exitonclick()
+
