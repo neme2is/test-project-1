19bfa4cdf1c17187f4326c97c4633cd9aa21b8b2
nl1644 <10424lisa@gmail.com>
2017-08-20 21:12:34 -0700

    Add files via upload

---
+++ drawFlower.py
@@ -0,0 +1,15 @@
+import turtle
+flower = turtle.Turtle()
+flower.color('blue')
+
+window = turtle.Screen()
+window.bgcolor('bisque')
+
+flower.forward(50)
+flower.right(120)
+flower.forward(50)
+flower.right(120)
+flower.forward(50)
+flower.right(120)
+
+window.exitonclick()
